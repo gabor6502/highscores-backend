db2e71c6f22b97b78c49284537641125fc224eee
gabor6502 <elan.gabor@gmail.com>
2025-07-21 13:05:27 -0500

    added and getting started with typeorm

---
+++ src/entity/Score.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class Score {
+
+    private MAX_USERNAME_LENGTH: number = 256
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ type: 'varchar', length: 256, unique: true }) // figure out how to link with constant above
+    username: string
+
+    @Column({ type: 'integer' })
+    score: number
+
+    @Column({ type: 'timestamptz' })
+    dateScored: Date
+
+    get maxUsernameLength()
+    {
+        return this.MAX_USERNAME_LENGTH;
+    }
+}
